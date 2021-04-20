13884cec55ee70a2059b7f090a165ea9eeeefd35
EricksonDutra <ericksond10@gmail.com>
2021-04-19 23:30:33 -0400

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { response } from "express";
+
+const app = express();
+
+app.get("/", (req,res) => {
+  return res.json({ message: "Olá NLW 05",});
+});
+
+app.post("/", (req, res) => {
+  return res.json({ 
+    message: "User success save!!",
+  })
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
