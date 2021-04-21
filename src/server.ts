6279f5c8d56855717a21e9348407eb730843d19b
EricksonDutra <ericksond10@gmail.com>
2021-04-20 20:13:24 -0400

    Finalizada aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import express, { response } from "express";
+import express from "express";
+import "./database";
+import { routes } from "./routes"
 
 const app = express();
 
-app.get("/", (req,res) => {
-  return res.json({ message: "Olá NLW 05",});
-});
+app.use(express.json());
 
-app.post("/", (req, res) => {
-  return res.json({ 
-    message: "User success save!!",
-  })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
